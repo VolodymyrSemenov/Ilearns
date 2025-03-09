b858f3040b654b3f1ecc89e4a095d16e239fe13d
Gabe C <tvcornel@buffalo.edu>
2025-03-08 22:34:48 -0500

    Button press code for 4 buttons connected to pins 2-5. check_button_pressed started and general flow started

--- src/ilearns_app.ino
+++ src/ilearns_app.ino
@@ -4,10 +4,24 @@
 #include <games/find_letters_annunciation.ino>
 #include <games/find_numbers.ino>
 
+// Pins
+const int button1 = 2;
+const int button2 = 3;
+const int button3 = 4;
+const int button4 = 5;
+
 void setup(){
     // Turn on speakers
     // Turn on NFC reading library
     // Define game pieces
+    
+    Serial.begin(9600);
+    pinMode(button1, INPUT_PULLUP);
+    pinMode(button2, INPUT_PULLUP);
+    pinMode(button3, INPUT_PULLUP);
+    pinMode(button4, INPUT_PULLUP);
+    =
+    Serial.println("Press a button to start a game.");
 
 }
 
@@ -23,22 +37,28 @@ void loop(){
     // the hint, repeat, skip, and fourth button on right side, 
     // the toggle switch next to a-z wand, and the reset button/switch
 
-    if (button_pressed){
-        if (button_number == 1){
-            begin_game_letter_ordering();
+    if (button_pressed) {
+        switch (button_number) {
+            case 1:
+                Serial.println("Starting Letter Ordering Game...");
+                // begin_game_letter_ordering();
+                break;
+            case 2:
+                Serial.println("Starting Number Ordering Game...");
+                // begin_game_number_ordering();
+                break;
+            case 3:
+                Serial.println("Starting Find Letters (Spoken) Game...");
+                // begin_game_find_letters_spoken();
+                break;
+            case 4:
+                Serial.println("Starting Find Letters (Annunciation) Game...");
+                // begin_game_find_letters_annunciation();
+                break;
+            // Add more buttons here: hint, repeat, end game, and skip button
+            default:
+                Serial.println("Unknown button pressed.");
         }
-        else if (button_number == 2){
-            // Call function
-        }
-        else if (button_number == 3){
-            // Call function
-        }
-        else if (button_number == 4){
-            // Call function
-        }
-        // Add more buttons here
-
-        // Handle button presses in each of the functions for a hint, repeat, and skip button
     }
 
 }
@@ -47,6 +67,19 @@ bool check_button_pressed(int &button_number){
     // Check if a button is pressed
     // If a button is pressed, return true and set button_number to the number of the button pressed
     // If no button is pressed, return false
+    if (digitalRead(button1) == LOW) {
+        button_number = 1;
+        return true;
+    } else if (digitalRead(button2) == LOW) {
+        button_number = 2;
+        return true;
+    } else if (digitalRead(button3) == LOW) {
+        button_number = 3;
+        return true;
+    } else if (digitalRead(button4) == LOW) {
+        button_number = 4;
+        return true;
+    }
     return false;
 }
 
