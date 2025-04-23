bcdf5652ab20dbdb281962bd90b246a3de429f74
Steven Collins <sac9@buffalo.edu>
2025-04-23 14:32:43 -0400

    deleted old main files, relocated some files

--- games/games.h
+++ games/games.h
@@ -3,7 +3,11 @@
 
 
 void begin_game_letter_ordering();
-void begin_game_number_ordering();
 void begin_game_find_letters_spoken();
 void begin_game_find_letters_annunciation();
+
+void begin_game_number_ordering();
 void begin_game_find_numbers();
+
+
+#endif
--- utils/button_handler.h
+++ utils/button_handler.h
@@ -2,21 +2,6 @@
 #include <Arduino.h>
 #include "constants.h"  // make sure this contains pin and state constants
 
-// External shared variables
-extern int game_state;
-extern bool game_over;
-extern const int annunciation_pin;
-
-extern const int letter_ordering_button;
-extern const int letter_wand_button;
-extern const int number_ordering_button;
-extern const int number_wand_button;
-extern const int hint_button;
-extern const int end_game_button;
-extern const int repeat_button;
-extern const int skip_button;
-extern const int recalibrate_button;
-
 // LED control
 void illuminate_button_led(int button_number);
 void deilluminate_button_led(int button_number);
